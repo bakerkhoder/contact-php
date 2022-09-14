5c5bdf8cd00f53726a14a041ce8fa14120a9021d
Baker Khoder <bakerkhoder878@gmail.com>
2022-09-14 06:35:06 +0300

    adding table and insertions

---
+++ backend/contacts.php
@@ -0,0 +1,12 @@
+<?php  
+include("connection.php");
+$query=$mysqli->prepare("SELECT name FROM contacts");
+$query->execute();
+$array=$query->get_result();
+$response=[];
+while($a =$array->fetch_assoc()){
+    $response[]=$a;
+}
+$json=json_encode($response);
+echo $json;
+?>
